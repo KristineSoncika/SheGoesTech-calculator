c649f1cb4d79f73c26b061ef5a318c7073082c68
kristine <57680335+kristtiine@users.noreply.github.com>
2022-03-12 21:16:52 +0200

    add more operators

---
+++ README.md
@@ -0,0 +1,9 @@
+# She Goes Tech JS Training - Practical Task
+
+Build a simple calculator using jQuery
+
+Live Site URL [**here**](https://kristinesoncika.github.io/SheGoesTech-calculator/)
+
+Finished project screenshot
+
+![Finished project screenshot](./screenshot.png)
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(() => {
   let num1 = "";
   let num2 = "";
   let operator = "";
-  let total = 0;
+  let total;
 
   // the first number is stored in num1; the second in num2
 
@@ -26,7 +26,7 @@ $(() => {
 
   // after total is stored in num1, and next number clicked in num2
   const updateNums = () => {
-    num1 = total.toString();
+    num1 = total;
     num2 = "";
   };
 
@@ -36,51 +36,79 @@ $(() => {
       case "+":
         total = +num1 + +num2;
         total = maxTenDec(total);
+        total = numberFormat(total);
         displayContent(total);
+        total = turnIntoNumber(total);
         break;
       case "-":
         total = +num1 - +num2;
         total = maxTenDec(total);
+        total = numberFormat(total);
         displayContent(total);
+        total = turnIntoNumber(total);
         break;
       case "/":
         total = +num1 / +num2;
         total = maxTenDec(total);
+        total = numberFormat(total);
         displayContent(total);
+        total = turnIntoNumber(total);
+        error();
         break;
       case "x":
         total = +num1 * +num2;
         total = maxTenDec(total);
+        total = numberFormat(total);
         displayContent(total);
+        total = turnIntoNumber(total);
         break;
     }
     updateNums();
   };
 
+  // if divided by zero, show error
+  const error = () => {
+    if (total === Infinity) {
+      return $(".screen").html("Error");
+    }
+  };
+
   // calculations based on the operator clicked (requires one number)
   const otherOperators = (op) => {
     if (op === "R2") {
-      total = parseInt((+num1).toFixed(2));
+      total = (+num1).toFixed(2);
+      total = numberFormat(total);
       displayContent(total);
+      total = turnIntoNumber(total);
     } else if (op === "R0") {
       total = Math.ceil(+num1);
+      total = numberFormat(total);
       displayContent(total);
+      total = turnIntoNumber(total);
     } else if (op === "%") {
       total = +num1 / 100;
       total = maxTenDec(total);
+      total = numberFormat(total);
       displayContent(total);
+      total = turnIntoNumber(total);
     } else if (op === "x²") {
       total = (+num1) ** 2;
       total = maxTenDec(total);
+      total = numberFormat(total);
       displayContent(total);
+      total = turnIntoNumber(total);
     } else if (op === "√x") {
       total = (+num1) ** 0.5;
       total = maxTenDec(total);
+      total = numberFormat(total);
       displayContent(total);
+      total = turnIntoNumber(total);
     } else if (op === "+/-") {
       total = +num1 * -1;
       total = maxTenDec(total);
+      total = numberFormat(total);
       displayContent(total);
+      total = turnIntoNumber(total);
     }
     updateNums();
     return total;
@@ -91,7 +119,7 @@ $(() => {
     $(".screen").html(btn);
   };
 
-  // adds numbers and operators to a calculation based on buttons clicked
+  // add numbers and operators to a calculation based on buttons clicked
   let value = "";
   $("button").on("click", (event) => {
     let btn = $(event.currentTarget).html();
@@ -138,30 +166,63 @@ $(() => {
     displayContent("");
   };
 
-  // sets max of 10 decimals after comma (but removes trailing zeros if any)
+  // add custom number format
+  const numberFormat = (num) => {
+    // create array of two substrings split by '.'
+    num = num.toString().split(".");
+    // insert space to 4th last position of the string
+    num[0] = num[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    // return the whole string
+    return (num = num.join("."));
+  };
+
+  // turn into number after the string is formatted
+  const turnIntoNumber = (num) => {
+    if (num.includes(" ")) {
+      num = num.replaceAll(" ", "");
+      num = parseFloat(num);
+      return num;
+    } else {
+      return num;
+    }
+  };
+
+  // set max of 10 decimals after comma (but removes trailing zeros if any)
   const maxTenDec = (num) => {
     if (!Number.isInteger(num)) {
       num = parseFloat(num.toFixed(10));
     }
     if ((!Number.isInteger(num) && num > 10) || num < -10) {
-      $(".screen").css("font-size", "3.3rem");
+      $(".screen").css("font-size", "3.2rem");
     }
     if ((!Number.isInteger(num) && num > 100) || num < -100) {
-      $(".screen").css("font-size", "3.1rem");
+      $(".screen").css("font-size", "3.0rem");
     }
     if ((!Number.isInteger(num) && num > 1000) || num < -1000) {
-      $(".screen").css("font-size", "2.9rem");
+      $(".screen").css("font-size", "2.8rem");
     }
     if ((!Number.isInteger(num) && num > 10000) || num < -10000) {
-      $(".screen").css("font-size", "2.7rem");
+      $(".screen").css("font-size", "2.6rem");
     }
     if ((!Number.isInteger(num) && num > 100000) || num < -100000) {
-      num = parseFloat(num.toFixed(9));
       $(".screen").css("font-size", "2.5rem");
+    }
+    if ((!Number.isInteger(num) && num > 1000000) || num < -1000000) {
+      num = parseFloat(num.toFixed(4));
+      $(".screen").css("font-size", "2.4rem");
+    }
+    if (
+      (!Number.isInteger(num) && num > 1000000000000) ||
+      num < -1000000000000
+    ) {
+      num = parseFloat(num.toFixed(2));
+      $(".screen").css("text-align", "left");
+      $(".screen-frame").css("justify-content", "left");
     }
     return num;
   };
 
+  // active buttons
   $(".content").on("click", (event) => {
     $(event.currentTarget).addClass("active-btn");
     $(event.currentTarget).siblings().removeClass("active-btn");
